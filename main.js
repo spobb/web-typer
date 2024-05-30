e3e334bb61ba7dc18d93b3ff85da95e13179f9ba
spobb <gverlaeken@gmail.com>
2024-05-30 22:37:17 +0200

    finished keyboard layout

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
-letters = 'azertyuiopqsdfghjklmwxcvbn';
-letters = letters.split('');
+document.addEventListener('DOMContentLoaded', () => {
+	let letters = 'azertyuiopqsdfghjklmwxcvbn';
+	const special = ['Tab', 'ControlLeft', 'ControlRight', 'ShiftLeft', 'ShiftRight', 'Enter', 'AltLeft', 'AltRight', 'CapsLock', 'BracketLeft'];
+	const wrapper = document.querySelector('.type');
+	letters = letters.split('');
 
-type = 'This is just a simple typing test.';
-type = type.split('');
-type.forEach((e) => {
-	document.write(`<span class="typeletter">${e}<span>`);
-});
+	let type = '';
+	type = type.split('');
+	type.forEach((e) => {
+		if (e == ' ') e = '&nbsp;';
+		const l = document.createElement('span');
+		l.classList.add('typeletter');
+		l.innerHTML = e;
+		document.querySelector('.type').appendChild(l);
+	});
 
-document.addEventListener('keydown', (e) => {
-	console.log(e.key);
-	document.querySelector('#' + e.key).classList.add('letter-pressed');
-	this.addEventListener('keyup', () => {
-		document.querySelector('#' + e.key).classList.remove('letter-pressed');
+	console.log(wrapper.childNodes.length);
+	document.addEventListener('keydown', (e) => {
+		console.log(e.code);
+		if (e.code == 'Backspace') {
+			if (wrapper.childNodes.length > 3) wrapper.removeChild(wrapper.childNodes[wrapper.childNodes.length - 3]);
+			return;
+		}
+		document.querySelector('#' + e.code).classList.add('letter-pressed');
+		this.addEventListener('keyup', () => {
+			document.querySelector('#' + e.code).classList.remove('letter-pressed');
+			return;
+		});
+		if (special.includes(e.code)) {
+			return;
+		}
+		const l = document.createElement('span');
+		l.classList.add('typeletter');
+		if (e.key == ' ') {
+			l.innerHTML = '&nbsp;';
+		} else {
+			l.innerHTML = e.key;
+		}
+		wrapper.insertBefore(l, document.querySelector('.cursor'));
 	});
 });
