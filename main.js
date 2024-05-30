3847d9a07a3c729f76c8c2a98f72091acf8401bf
spobb <gverlaeken@gmail.com>
2024-05-31 00:24:38 +0200

    implemented type game

--- main.js
+++ main.js
@@ -1,41 +1,97 @@
 document.addEventListener('DOMContentLoaded', () => {
-	let letters = 'azertyuiopqsdfghjklmwxcvbn';
 	const special = ['Tab', 'ControlLeft', 'ControlRight', 'ShiftLeft', 'ShiftRight', 'Enter', 'AltLeft', 'AltRight', 'CapsLock', 'BracketLeft'];
 	const wrapper = document.querySelector('.type');
-	letters = letters.split('');
+	const wpm = document.querySelector('.wpm');
 
-	let type = '';
-	type = type.split('');
-	type.forEach((e) => {
-		if (e == ' ') e = '&nbsp;';
+	let timer = 0;
+
+	let string = 'HEHEHeHA';
+
+	// creates letters in DOM
+
+	const toType = string.split('');
+	toType.forEach((letter) => {
+		if (letter == ' ') letter = '&nbsp;';
 		const l = document.createElement('span');
-		l.classList.add('typeletter');
-		l.innerHTML = e;
+		l.classList.add('totype');
+		l.innerHTML = letter;
 		document.querySelector('.type').appendChild(l);
 	});
 
-	console.log(wrapper.childNodes.length);
+	function startTimer() {
+		return Date.now();
+	}
+
+	function getWPM(start) {
+		let curr = Date.now();
+		return Math.floor(i / 5 / ((curr - start) / 60000));
+	}
+
+	const letters = document.querySelectorAll('.totype');
+
+	let i = 0;
+	let TIMED = false;
+
 	document.addEventListener('keydown', (e) => {
-		console.log(e.code);
+		//handle keystrokes and style them as pressed
+		if (!TIMED) {
+			timer = startTimer();
+			TIMED = true;
+		}
+		wpm.innerText = 'WPM:' + getWPM(timer);
+
+		// delete letter if key is backspace
 		if (e.code == 'Backspace') {
-			if (wrapper.childNodes.length > 3) wrapper.removeChild(wrapper.childNodes[wrapper.childNodes.length - 3]);
+			// if (letters.length > 0) wrapper.removeChild(letters[letters.length - 1]);
+			--i;
+			letters[i].classList.remove('wrong');
+			letters[i].classList.remove('checked');
+			i < letters.length ? letters[i + 1].classList.remove('current') : null;
+			letters[i].classList.add('current');
+			if (toType[i] == ' ') {
+				letters[i].innerHTML = '&nbsp;';
+				return;
+			}
+			letters[i].innerText = toType[i];
 			return;
 		}
+
+		// ignore key if it is special
+		if (special.includes(e.code)) {
+			return;
+		}
+
+		if (e.key == ' ') {
+			e.key = '&nbsp;';
+		}
+
+		if (toType[i] === e.key) {
+			letters[i].classList.add('checked');
+		} else {
+			letters[i].classList.add('wrong');
+			letters[i].innerText = e.key;
+		}
+		i < letters.length ? letters[i + 1].classList.add('current') : null;
+		letters[i].classList.remove('current');
+
+		const l = document.createElement('span');
+
+		++i;
+		// l.classList.add('typeletter');
+
+		// wrapper.insertBefore(l, document.querySelector('.cursor'));
+
+		// style keyboard
+
 		document.querySelector('#' + e.code).classList.add('letter-pressed');
 		this.addEventListener('keyup', () => {
 			document.querySelector('#' + e.code).classList.remove('letter-pressed');
 			return;
 		});
-		if (special.includes(e.code)) {
-			return;
+
+		if (i > letters.length) {
+			document.write('VOUS AVEZ FINI!!!!!!!!!!!!!!!!!!!!!!!!');
+			stopTimer();
 		}
-		const l = document.createElement('span');
-		l.classList.add('typeletter');
-		if (e.key == ' ') {
-			l.innerHTML = '&nbsp;';
-		} else {
-			l.innerHTML = e.key;
-		}
-		wrapper.insertBefore(l, document.querySelector('.cursor'));
 	});
 });
